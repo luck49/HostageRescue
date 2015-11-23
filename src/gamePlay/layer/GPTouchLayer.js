52323557f2bc12ba9e6a71fd7509981a91e33455
LJJ <476953950@qq.com>
2015-11-23 11:42:06 +0800

    add something

--- src/gamePlay/layer/GPTouchLayer.js
+++ src/gamePlay/layer/GPTouchLayer.js
@@ -11,6 +11,32 @@ var GPTouchLayer = cc.Layer.extend({
 
 
 
+    },
+
+
+    initListen : function(target){
+        var listener = cc.EventListener.create({
+            event: cc.EventListener.TOUCH_ONE_BY_ONE,
+            swallowTouches : false,
+            onTouchBegan: function (touch, event) {
+                var target = event.getCurrentTarget();
+                cc.log("onTouch:",target);
+                if (!target.isTouchInPlate(touch)){
+                    return false;
+                }
+                cc.log("onTouch");
+                return true;
+            },
+            onTouchEnded: function (touch, event) {
+                cc.log("onTouchend");
+            },
+            onTouchCancelled : function(touch, event){
+                cc.log("onTouchCancelled");
+            }
+        });
+        cc.eventManager.addListener(listener, target);
+
+
     }
 
 
